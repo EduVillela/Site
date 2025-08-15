60efb02856fe2a53fa72a50f971f811e002a0bd6
Sergio Herman <sergioherman@id.uff.br>
2025-08-14 23:32:27 -0300

    js para abrir e fechar o menu, rolagem do menu para telas verticalmente pequenas, estilização do scrollbar da pagina inteira

--- js/menu.js
+++ js/menu.js
@@ -1,2 +1,19 @@
 const menu = document.querySelector('.mobile-menu');
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.mobile-menu');
+  
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('active');
+
+        // Alternar o ícone
+        if (menu.classList.contains('active')) {
+            hamburger.innerHTML = '&#10005;'; // ✖ (X)
+        } else {
+            hamburger.innerHTML = '&#9776;'; // ☰ (hambúrguer)
+        }
+
+    });
+});
